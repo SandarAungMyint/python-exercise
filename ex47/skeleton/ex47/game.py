053d5dd2a977a81cc80544492a564c79b42ec361
SandarAungMyint <sandar27141995@gmail.com>
2018-06-20 09:56:34 +0630

    Add ex46 and ex47

---
+++ ex47/skeleton/ex47/game.py
@@ -0,0 +1,13 @@
+
+class Room(object):
+
+    def__init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.path = []
+
+    def go(self, direction):
+        return self.paths.get(direction, None)
+
+    def add_paths(self, paths):
+        self.paths.update(paths)
