26096df1874b2d1819bcbef4c4bdbb05bafda53e
SandarAungMyint <sandar27141995@gmail.com>
2018-06-26 12:31:14 +0630

    update

--- ex41.py
+++ ex41.py
@@ -1,61 +1,59 @@
-import random 
-from urllib import urlopen
+import random
+from urllib  import urlopen
 import sys
 
-WORLD_URL = "http://learncodethehardway.org/words.txt"
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
-    "class %%%(%%%):":
-      "Make a class named %%% that is-a %%%",
-    "class %%%(object):\n\tdef_init_(self, ***)":
-      "class %%% has-a _init_that takes self and *** parameters.",
-    "class %%%(object):\n\tdef ***(self, @@@)":
-      "class %%% has-a function named *** that takes self and @@@ parameters.",
+    "class %%%(%%%)":
+        "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\t def __init__(self,***)":
+        "class %%% has-a __init__ that takes self and *** parameters",
+    "class %%%(object):\n\tdef ***(self,@@@)":
+        "class %%% has-a function *** that takes self and @@@",
     "*** = %%%()":
-      "Set *** to an instance of class %%%.",
+        "Set ** to an instance of class %%%.",
     "***.***(@@@)":
-      "From *** get the *** function, and call it with parameters self, @@@.",
+        "From *** get the *** function, call it with params set",
     "***.*** = '***'":
-      "From *** get the *** attribute and set it to '***'"
-      
+        "From *** get the *** attribute and set it to '***'"
 }
-
-# do they want to drill phrases first
+# do they wamt to drill phrases first
 PHRASE_FIRST = False
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST = True
 
-# load up the words from the website
+# load up the worlds from the website
 for word in urlopen(WORD_URL).readlines():
     WORDS.append(word.strip())
 
+
 def convert(snippet, phrase):
-    class_names = [w.capatilize() for w in
-                    random.sample(WORDS, snippet.count("%%%"))]
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]
     other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
     for i in range(0, snippet.count("@@@")):
-        param_count = random.randint(1,3)
-        param_names.append(','.join(random.sample(WORDS, param_count)))
+        param_count = random.randint(1, 3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
-
-       result = sentence[:]
+        result = sentence[:]
 
         # fake class names
         for word in class_names:
-          result = result.replace("%%%", word, 1)
+            result = result.replace("%%%", word, 1)
 
         # fake other names
         for word in other_names:
-          result = result.replace("***", word, 1)
+            result = result.replace("***", word, 1)
 
-        # fake parameter lists
+       # fake parameter lists
         for word in param_names:
-           result = result.replace("@@@", word, 1)
+            result = result.replace("@@@", word, 1)
 
         results.append(result)
 
@@ -65,19 +63,19 @@ def convert(snippet, phrase):
 # keep going until they hit CTRL-D
 try:
     while True:
-        snippets = PHRASES.keys()
+        snippets = list(PHRASES.keys())
         random.shuffle(snippets)
 
         for snippet in snippets:
             phrase = PHRASES[snippet]
             question, answer = convert(snippet, phrase)
             if PHRASE_FIRST:
-               question, answer = answer, question
-
+                question, answer = answer, question
             print question
 
+
             raw_input("> ")
-            print "ANSWER: %s\n\n" %answer
+        print "ANSWER : %s\n\n" % answer
 
 except EOFError:
     print "\nBye"
