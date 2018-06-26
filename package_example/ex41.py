5dd40b6bae95932771967ae1974322445e2216f0
SandarAungMyint <sandar27141995@gmail.com>
2018-06-26 14:18:59 +0630

    done

--- package_example/ex41.py
+++ package_example/ex41.py
@@ -45,16 +45,16 @@ def convert(snippet, phrase):
 
        result = sentence[:]
 
-        # fake class names
-        for word in class_names:
+     # fake class names
+    for word in class_names:
           result = result.replace("%%%", word, 1)
 
-        # fake other names
-        for word in other_names:
+      # fake other names
+    for word in other_names:
           result = result.replace("***", word, 1)
 
-        # fake parameter lists
-        for word in param_names:
+       # fake parameter lists
+     for word in param_names:
            result = result.replace("@@@", word, 1)
 
         results.append(result)
